25e528867acd0ccafed2439511890aca9ab793e7
Laz <rosspetridis@gmail.com>
2022-09-17 04:45:06 +0000

    unit tests WIP

--- Ref/MathSender/MathSender.cpp
+++ Ref/MathSender/MathSender.cpp
@@ -42,10 +42,10 @@ namespace Ref {
   // ----------------------------------------------------------------------
 
   void MathSender ::
-    mathResultIn_handler(
+    mathResultIn_handler( // called when MathReceiver component code returns a result by invoking the mathResultIn port.
         const NATIVE_INT_TYPE portNum,
         F32 result
-    )
+    ) 
   {
     this->tlmWrite_RESULT(result);
     this->log_ACTIVITY_HI_RESULT(result);
@@ -57,21 +57,19 @@ namespace Ref {
 
   void MathSender ::
     DO_MATH_cmdHandler(
-        const FwOpcodeType opCode,
-        const U32 cmdSeq,
-        F32 val1,
+        const FwOpcodeType opCode, // command opcode
+        const U32 cmdSeq,          // Command Sequence number
+        F32 val1,           // remaining args supplied when command is sent.
         Ref::MathOp op,
         F32 val2
     )
   {
-
     this->tlmWrite_VAL1(val1);
     this->tlmWrite_OP(op);
     this->tlmWrite_VAL2(val2);
     this->log_ACTIVITY_LO_COMMAND_RECV(val1, op, val2);
     this->mathOpOut_out(0, val1, op, val2);
-    this->cmdResponse_out(opCode, cmdSeq, Fw::CmdResponse::OK);
-
+    this->cmdResponse_out(opCode, cmdSeq, Fw::CmdResponse::OK); // all handlers need to end with cmd resposne.
   }
 
 } // end namespace Ref
