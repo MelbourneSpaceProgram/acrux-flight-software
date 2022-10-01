338543006415a932c000aa991b9df2b7882e7ca2
Lazaros <rosspetridis@gmail.com>
2022-10-01 06:03:40 +0000

    I2C In handler initial pairing implementation

--- Ref/I2CAdapter/I2CAdapter.cpp
+++ Ref/I2CAdapter/I2CAdapter.cpp
@@ -41,16 +41,6 @@ namespace Ref {
   // Handler implementations for user-defined typed input ports
   // ----------------------------------------------------------------------
 
-
-
-
-
-
-
-
-
-
-
   void I2CAdapter ::
     adapterIn_handler(
         const NATIVE_INT_TYPE portNum,
@@ -59,6 +49,17 @@ namespace Ref {
   {
     // TODO - write to the port.
 
+    //get accelerometer data to x y and z based on python code https://www.engineersgarage.com/adxl345-accelerometer-raspberry-pi-i2c/
+    Fw::Buffer send; // Can use Fw (Framework)
+    
+    char data[4];
+    data = val;
+    addr = portNum; // TODO rename portNum. ?
+
+    send.setSize(4);
+    send.setData((U8*)data);
+    
+    this->I2c_out(0,addr,send);
   }
 
 } // end namespace Ref
