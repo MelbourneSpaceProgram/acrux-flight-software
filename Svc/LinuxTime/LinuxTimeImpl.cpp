5219fc04cf59b3cc58f73b5468c8cebfd18fb183
bill <toyjet8@gmail.com>
2022-10-27 20:55:42 +1100

    reimplemented LinuxTime for freeRTOS taking into account uint32_t overflow

--- Svc/LinuxTime/LinuxTimeImpl.cpp
+++ Svc/LinuxTime/LinuxTimeImpl.cpp
@@ -27,10 +27,15 @@ namespace Svc
     {
         // tick_count - ms
         // TODO(Daniel/Bill): Fix this to avoid overflow
-        TickType_t tick_count = xTaskGetTickCount();
-        uint32_t time_seconds = tick_count / 1000;
-        uint32_t time_microseconds = time_seconds * 1000000;
-        time.set(TB_WORKSTATION_TIME, 0, time_seconds, time_microseconds);
+        TickType_t tick_count = xTaskGetTickCount(); //assume 32bit tick counts
+        uint64_t new_sys_time_usec = tick_count*1000;
+        
+        uint64_t time_total_usec = time.getSeconds ()*1000000 + time.getUSeconds ();
+        uint32_t new_time_total_usec = new_sys_time_usec + (new_sys_time_usec - time_total_usec%(UINT32_MAX*1000)); //to handle rollover in 32bit tick counts
+
+        uint32_t new_time_sec = new_time_total_usec/1000000; //calculate the second portion
+        uint32_t new_time_usec = new_time_total_usec%1000000; //calculate the microsecond portion
+        time.set(TB_WORKSTATION_TIME, 0, new_time_sec, new_time_usec);
     }
 
     void LinuxTimeImpl::init(NATIVE_INT_TYPE instance)
